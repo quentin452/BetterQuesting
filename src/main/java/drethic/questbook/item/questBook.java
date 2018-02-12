ff554e2d4a53e9ebcfba6a23f8092217695ce2c5
Dream-Master <dream-master@gmx.net>
2018-02-12 12:51:18 +0100

    Alphest fix

--- src/main/java/drethic/questbook/item/questBook.java
+++ src/main/java/drethic/questbook/item/questBook.java
@@ -1,8 +1,8 @@
 package drethic.questbook.item;
 
 import betterquesting.client.gui.GuiHome;
+import betterquesting.client.gui.GuiQuesting;
 import betterquesting.core.BetterQuesting;
-import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import drethic.questbook.QuestBook;
@@ -12,40 +12,35 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-public class questBook extends Item{
-	
-	public static boolean hasEffect;
-	
-	public questBook()
-    {
-    	this.setTextureName(QuestBook.MODID + ":questBook1-labled");
-    	this.setUnlocalizedName("questBook");
-    	this.setCreativeTab(BetterQuesting.tabQuesting);
-    	hasEffect = false;
-    }
+public class questBook extends Item {
 
-	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
-    {
-        return false;
-    }
-    
+	public static boolean hasEffect;
+
+	public questBook() {
+		this.setTextureName(QuestBook.MODID + ":questBook1-labled");
+		this.setUnlocalizedName("questBook");
+		this.setCreativeTab(BetterQuesting.tabQuesting);
+		hasEffect = false;
+	}
+
 	@Override
-    @SideOnly(Side.CLIENT)
-    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
-    {
-    	if(world.isRemote)
-    	{
-    		Minecraft mc = Minecraft.getMinecraft();
-			mc.displayGuiScreen(new GuiHome(mc.currentScreen));
-    	}
-    	
-        return stack;
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean hasEffect(ItemStack stack)
-    {
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+		return false;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		if (world.isRemote) {
+			Minecraft.getMinecraft().displayGuiScreen(GuiQuesting.getLastScreen() != null ? GuiQuesting.getLastScreen() : GuiHome.getInstance());
+		}
+
+		return stack;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean hasEffect(ItemStack stack) {
 		return hasEffect;
-    }
+	}
 }
