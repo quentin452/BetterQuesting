f722cecfa0a566630a9a67e630e10c79d4a4e557
SKProCH <shadowkillerpropro@yandex.ru>
2020-03-29 18:01:54 +0300

    Rework PanelRewardCommand

--- src/main/java/bq_standard/client/gui/rewards/PanelRewardCommand.java
+++ src/main/java/bq_standard/client/gui/rewards/PanelRewardCommand.java
@@ -6,6 +6,7 @@ import betterquesting.api2.client.gui.misc.GuiPadding;
 import betterquesting.api2.client.gui.misc.GuiTransform;
 import betterquesting.api2.client.gui.misc.IGuiRect;
 import betterquesting.api2.client.gui.panels.CanvasEmpty;
+import betterquesting.api2.client.gui.panels.CanvasMinimum;
 import betterquesting.api2.client.gui.panels.content.PanelGeneric;
 import betterquesting.api2.client.gui.panels.content.PanelTextBox;
 import betterquesting.api2.client.gui.resources.textures.ItemTexture;
@@ -15,25 +16,29 @@ import bq_standard.rewards.RewardCommand;
 import net.minecraft.init.Blocks;
 import org.lwjgl.util.vector.Vector4f;
 
-public class PanelRewardCommand extends CanvasEmpty
+public class PanelRewardCommand extends CanvasMinimum
 {
     private final IQuest quest;
     private final RewardCommand reward;
+    private final IGuiRect initialRect;
     
     public PanelRewardCommand(IGuiRect rect, IQuest quest, RewardCommand reward)
     {
         super(rect);
         this.quest = quest;
         this.reward = reward;
+        initialRect = rect;
     }
     
     @Override
     public void initPanel()
     {
         super.initPanel();
-        
-        this.addPanel(new PanelGeneric(new GuiTransform(new Vector4f(0F, 0.5F, 0F, 0.5F), 0, -16, 32, 32, 0), new ItemTexture(new BigItemStack(Blocks.command_block))));
+        int width = initialRect.getWidth();
+        this.addPanel(new PanelGeneric(new GuiTransform(new Vector4f(0F, 0F, 0F, 0F), 0, 0, 32, 32, 0), new ItemTexture(new BigItemStack(Blocks.command_block))));
         String txt = QuestTranslation.translate("advMode.command") + "\n\n" + (reward.hideCmd ? "[HIDDEN]" : reward.command);
-        this.addPanel(new PanelTextBox(new GuiTransform(new Vector4f(0F, 0.5F, 1F, 0.5F), new GuiPadding(36, -16, 0, -16), 0), txt).setColor(PresetColor.TEXT_MAIN.getColor()));
+        // TODO: Fix full text display by implementing normal measurement of the minimum required text height
+        this.addPanel(new PanelTextBox(new GuiTransform(new Vector4f(0F, 0F, 0F, 0F), 40, 0, width - 40, 64, 0), txt).setColor(PresetColor.TEXT_MAIN.getColor()));
+        recalcSizes();
     }
 }
