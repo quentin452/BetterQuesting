e8e44662e393663f986e5ec4d95f75f63947d4a4
SKProCH <skproch@yandex.ru>
2021-01-02 20:25:07 +0300

    Added a bypass that allows you to return to the quest book after viewing a recipe in NEI

---
+++ src/main/java/bq_standard/client/gui/GuiContainerFake.java
@@ -0,0 +1,33 @@
+package bq_standard.client.gui;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+
+import java.util.function.Consumer;
+
+public class GuiContainerFake extends GuiContainer {
+    public GuiContainerFake() {
+        super(null);
+    }
+
+    private Consumer onInitCallback;
+
+    @Override
+    public void initGui() {
+        if (onInitCallback != null)
+            onInitCallback.accept(null);
+    }
+
+    @Override
+    public void onGuiClosed() {
+
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) {
+
+    }
+
+    public void setOnInitCallback(Consumer onInitCallback) {
+        this.onInitCallback = onInitCallback;
+    }
+}
