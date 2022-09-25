53de0a1a208110fdb7dcc5e58592fbce25ddde34
ALongStringOfNumbers <31759736+alongstringofnumbers@users.noreply.github.com>
2022-09-25 11:33:44 +0800

    Mark the player dirty after using the quest reset button (CleanroomMC/BetterQuesting#56)

--- src/main/java/betterquesting/client/toolbox/tools/ToolboxToolReset.java
+++ src/main/java/betterquesting/client/toolbox/tools/ToolboxToolReset.java
@@ -3,8 +3,10 @@ package betterquesting.client.toolbox.tools;
 import betterquesting.api.client.toolbox.IToolboxTool;
 import betterquesting.api2.client.gui.controls.PanelButtonQuest;
 import betterquesting.api2.client.gui.panels.lists.CanvasQuestLine;
+import betterquesting.api2.utils.DirtyPlayerMarker;
 import betterquesting.client.gui2.editors.designer.PanelToolController;
 import betterquesting.network.handlers.NetQuestEdit;
+import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.NBTTagCompound;
 import org.lwjgl.input.Keyboard;
 
@@ -14,7 +16,7 @@ import java.util.List;
 public class ToolboxToolReset implements IToolboxTool
 {
 	private CanvasQuestLine gui;
-	
+
 	@Override
 	public void initTool(CanvasQuestLine gui)
 	{
@@ -25,39 +27,40 @@ public class ToolboxToolReset implements IToolboxTool
 	public void disableTool()
 	{
 	}
-	
+
 	@Override
     public void refresh(CanvasQuestLine gui)
     {
     }
-	
+
 	@Override
 	public boolean onMouseClick(int mx, int my, int click)
 	{
 		if(click != 0 || !gui.getTransform().contains(mx, my)) return false;
-		
+
 		PanelButtonQuest btn = gui.getButtonAt(mx, my);
-		
+
 		if(btn == null) return false;
 		if(PanelToolController.selected.size() > 0 && !PanelToolController.selected.contains(btn)) return false;
-		
+
         List<PanelButtonQuest> btnList = PanelToolController.selected.size() > 0 ? PanelToolController.selected : Collections.singletonList(btn);
         int[] questIDs = new int[btnList.size()];
-        
+
         for(int i = 0; i < btnList.size(); i++)
         {
             questIDs[i] = btnList.get(i).getStoredValue().getID();
         }
-        
+
         NBTTagCompound payload = new NBTTagCompound();
         payload.setIntArray("questIDs", questIDs);
         payload.setBoolean("state", false);
         payload.setInteger("action", 2);
         NetQuestEdit.sendEdit(payload);
-		
+        DirtyPlayerMarker.markDirty(Minecraft.getMinecraft().thePlayer.getUniqueID());
+
 		return true;
 	}
-	
+
 	@Override
     public boolean onMouseRelease(int mx, int my, int click)
     {
@@ -68,12 +71,12 @@ public class ToolboxToolReset implements IToolboxTool
 	public void drawCanvas(int mx, int my, float partialTick)
 	{
 	}
-	
+
 	@Override
     public void drawOverlay(int mx, int my, float partialTick)
     {
     }
-    
+
     @Override
     public List<String> getTooltip(int mx, int my)
     {
@@ -90,21 +93,22 @@ public class ToolboxToolReset implements IToolboxTool
 	public boolean onKeyPressed(char c, int key)
 	{
 	    if(PanelToolController.selected.size() <= 0 || key != Keyboard.KEY_RETURN) return false;
-	    
+
         List<PanelButtonQuest> btnList = PanelToolController.selected;
         int[] questIDs = new int[btnList.size()];
-        
+
         for(int i = 0; i < btnList.size(); i++)
         {
             questIDs[i] = btnList.get(i).getStoredValue().getID();
         }
-        
+
         NBTTagCompound payload = new NBTTagCompound();
         payload.setIntArray("questIDs", questIDs);
         payload.setBoolean("state", false);
         payload.setInteger("action", 2);
         NetQuestEdit.sendEdit(payload);
-        
+        DirtyPlayerMarker.markDirty(Minecraft.getMinecraft().thePlayer.getUniqueID());
+
         return true;
 	}
 
@@ -113,12 +117,12 @@ public class ToolboxToolReset implements IToolboxTool
 	{
 		return true;
 	}
-	
+
 	@Override
     public void onSelection(List<PanelButtonQuest> buttons)
     {
     }
-	
+
 	@Override
     public boolean useSelection()
     {
