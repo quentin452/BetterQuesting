9f3cc56cb60191b1542e1254b1e2d03416af28fc
D-Cysteine <54219287+D-Cysteine@users.noreply.github.com>
2023-02-17 22:47:08 -0700

    Update network handlers

--- src/main/java/bq_standard/network/handlers/NetRewardChoice.java
+++ src/main/java/bq_standard/network/handlers/NetRewardChoice.java
@@ -7,6 +7,7 @@ import betterquesting.api.events.DatabaseEvent.DBType;
 import betterquesting.api.network.QuestingPacket;
 import betterquesting.api.questing.IQuest;
 import betterquesting.api.questing.rewards.IReward;
+import betterquesting.api.utils.NBTConverter;
 import betterquesting.api2.utils.Tuple2;
 import bq_standard.core.BQ_Standard;
 import bq_standard.rewards.RewardChoice;
@@ -20,6 +21,9 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public class NetRewardChoice {
     private static final ResourceLocation ID_NAME = new ResourceLocation("bq_standard:choice_reward");
 
@@ -32,17 +36,15 @@ public class NetRewardChoice {
     }
 
     @SideOnly(Side.CLIENT)
-    public static void requestChoice(int questID, int rewardID, int index) {
-        NBTTagCompound payload = new NBTTagCompound();
-        payload.setInteger("questID", questID);
+    public static void requestChoice(UUID questID, int rewardID, int index) {
+        NBTTagCompound payload = NBTConverter.writeQuestId(questID);
         payload.setInteger("rewardID", rewardID);
         payload.setInteger("selection", index);
         QuestingAPI.getAPI(ApiReference.PACKET_SENDER).sendToServer(new QuestingPacket(ID_NAME, payload));
     }
 
-    public static void sendChoice(@Nonnull EntityPlayerMP player, int questID, int rewardID, int index) {
-        NBTTagCompound payload = new NBTTagCompound();
-        payload.setInteger("questID", questID);
+    public static void sendChoice(@Nonnull EntityPlayerMP player, UUID questID, int rewardID, int index) {
+        NBTTagCompound payload = NBTConverter.writeQuestId(questID);
         payload.setInteger("rewardID", rewardID);
         payload.setInteger("selection", index);
         QuestingAPI.getAPI(ApiReference.PACKET_SENDER).sendToPlayers(new QuestingPacket(ID_NAME, payload), player);
@@ -52,19 +54,22 @@ public class NetRewardChoice {
         EntityPlayerMP sender = message.getSecond();
         NBTTagCompound tag = message.getFirst();
 
-        int qID = tag.hasKey("questID") ? tag.getInteger("questID") : -1;
+        Optional<UUID> qID = NBTConverter.tryReadQuestId(tag);
         int rID = tag.hasKey("rewardID") ? tag.getInteger("rewardID") : -1;
         int sel = tag.hasKey("selection") ? tag.getInteger("selection") : -1;
 
-        if (qID < 0 || rID < 0) return;
+        if (!qID.isPresent() || rID < 0)
+        {
+            return;
+        }
 
-        IQuest quest = QuestingAPI.getAPI(ApiReference.QUEST_DB).getValue(qID);
+        IQuest quest = QuestingAPI.getAPI(ApiReference.QUEST_DB).get(qID.get());
         IReward reward = quest == null ? null : quest.getRewards().getValue(rID);
 
         if (reward instanceof RewardChoice) {
             RewardChoice rChoice = (RewardChoice) reward;
             rChoice.setSelection(QuestingAPI.getQuestingUUID(sender), sel);
-            sendChoice(sender, qID, rID, sel);
+            sendChoice(sender, qID.get(), rID, sel);
         }
     }
 
@@ -72,13 +77,16 @@ public class NetRewardChoice {
     private static void onClient(NBTTagCompound message) {
         EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
 
-        int qID = message.hasKey("questID", 99) ? message.getInteger("questID") : -1;
+        Optional<UUID> qID = NBTConverter.tryReadQuestId(message);
         int rID = message.hasKey("rewardID", 99) ? message.getInteger("rewardID") : -1;
         int sel = message.hasKey("selection", 99) ? message.getInteger("selection") : -1;
 
-        if (qID < 0 || rID < 0) return;
+        if (!qID.isPresent() || rID < 0)
+        {
+            return;
+        }
 
-        IQuest quest = QuestingAPI.getAPI(ApiReference.QUEST_DB).getValue(qID);
+        IQuest quest = QuestingAPI.getAPI(ApiReference.QUEST_DB).get(qID.get());
         IReward reward = quest == null ? null : quest.getRewards().getValue(rID);
 
         if (reward instanceof RewardChoice) {
