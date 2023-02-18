3ad4254f039719760aaf5f6b92e9f0f6b028fa4e
D-Cysteine <54219287+D-Cysteine@users.noreply.github.com>
2023-02-18 16:18:34 -0700

    Add translation methods for names and descriptions

--- src/main/java/betterquesting/api2/utils/QuestTranslation.java
+++ src/main/java/betterquesting/api2/utils/QuestTranslation.java
@@ -1,18 +1,112 @@
 package betterquesting.api2.utils;
 
+import betterquesting.api.properties.IPropertyContainer;
+import betterquesting.api.properties.IPropertyType;
+import betterquesting.api.properties.NativeProps;
+import betterquesting.api.questing.IQuest;
+import betterquesting.api.questing.IQuestLine;
+import betterquesting.api2.storage.DBEntry;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.client.resources.Locale;
 
-public class QuestTranslation
-{
-    public static String translate(String text, Object... args)
-    {
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.UUID;
+
+public class QuestTranslation {
+    private static final String QUEST_NAME_KEY = "betterquesting.quest.%s.name";
+    private static final String QUEST_DESCRIPTION_KEY = "betterquesting.quest.%s.desc";
+    private static final String QUEST_LINE_NAME_KEY = "betterquesting.questline.%s.name";
+    private static final String QUEST_LINE_DESCRIPTION_KEY = "betterquesting.questline.%s.desc";
+
+    /**
+     * We'll look up translation keys directly from the map, to avoid needing to perform string
+     * comparison to check if a key is missing.
+     */
+    private static final Map<String, String> translations;
+    static {
+        try {
+            Field localeField = ReflectionHelper.findField(I18n.class, "i18nLocale");
+            Field translationsField = ReflectionHelper.findField(Locale.class, "field_135032_a");
+            translations = (Map<String, String>) translationsField.get(localeField.get(null));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String translate(String text, Object... args) {
         String out = I18n.format(text, args);
-        if(out.startsWith("Format error: ")) return text; // TODO: Find a more reliable way of detecting translation failure
+        if (out.startsWith("Format error: ")) {
+            return text; // TODO: Find a more reliable way of detecting translation failure
+        }
         return out;
     }
     
-    public static String translateTrimmed(String text, Object... args)
-    {
+    public static String translateTrimmed(String text, Object... args) {
         return translate(text, args).replaceAll("\r", "");
+    }
+
+    public static String buildQuestNameKey(UUID questId) {
+        return String.format(QUEST_NAME_KEY, questId);
+    }
+
+    public static String translateQuestName(UUID questId, IQuest quest) {
+        return translateProperty(buildQuestNameKey(questId), quest, NativeProps.NAME);
+    }
+
+    public static String translateQuestName(Map.Entry<UUID, IQuest> entry) {
+        return translateQuestName(entry.getKey(), entry.getValue());
+    }
+
+    public static String buildQuestDescriptionKey(UUID questId) {
+        return String.format(QUEST_DESCRIPTION_KEY, questId);
+    }
+
+    public static String translateQuestDescription(UUID questId, IQuest quest) {
+        return translateProperty(buildQuestDescriptionKey(questId), quest, NativeProps.DESC);
+    }
+
+    public static String translateQuestDescription(Map.Entry<UUID, IQuest> entry) {
+        return translateQuestDescription(entry.getKey(), entry.getValue());
+    }
+
+    public static String buildQuestLineNameKey(int questLineId) {
+        return String.format(QUEST_LINE_NAME_KEY, questLineId);
+    }
+
+    public static String translateQuestLineName(int questLineId, IQuestLine questLine) {
+        return translateProperty(buildQuestLineNameKey(questLineId), questLine, NativeProps.NAME);
+    }
+
+    public static String translateQuestLineName(DBEntry<IQuestLine> entry) {
+        return translateQuestLineName(entry.getID(), entry.getValue());
+    }
+
+    public static String buildQuestLineDescriptionKey(int questLineId) {
+        return String.format(QUEST_LINE_DESCRIPTION_KEY, questLineId);
+    }
+
+    public static String translateQuestLineDescription(int questLineId, IQuestLine questLine) {
+        return translateProperty(
+                buildQuestLineDescriptionKey(questLineId), questLine, NativeProps.DESC);
+    }
+
+    public static String translateQuestLineDescription(DBEntry<IQuestLine> entry) {
+        return translateQuestLineDescription(entry.getID(), entry.getValue());
+    }
+
+    /**
+     * Returns the translation, if one exists for {@code key}.
+     * If no translation exists, then {@code property} is fetched from {@code container}.
+     */
+    private static String translateProperty(
+            String key, IPropertyContainer container, IPropertyType<String> property) {
+        String translation = translations.get(key);
+        if (translation != null) {
+            return translation;
+        }
+
+        return container.getProperty(property);
     }
 }
