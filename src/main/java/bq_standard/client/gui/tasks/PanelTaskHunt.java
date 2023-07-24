ac826a94fbc28a39bd4d5e4289cc6091990d2de9
FindMeSomeFun <129369873+FindMeSomeFun@users.noreply.github.com>
2023-07-24 09:29:58 +0200

    Adapted Wisp entity preview. (#112)
    * Adapted Wisp entity preview.
    
    * Adapted Wisp entity preview.

--- src/main/java/bq_standard/client/gui/tasks/PanelTaskHunt.java
+++ src/main/java/bq_standard/client/gui/tasks/PanelTaskHunt.java
@@ -48,10 +48,16 @@ public class PanelTaskHunt extends CanvasMinimum {
                 .setAlignment(1)
                 .setColor(PresetColor.TEXT_MAIN.getColor()));
 
-        if (target != null)
+        if (target != null) {
+            ValueFuncIO<Float> pitch;
+            if (task.idName.contains("Wisp")) {
+                pitch = new ValueFuncIO<>(() -> 90F);
+            } else {
+                pitch = new ValueFuncIO<>(() -> 15F);
+            }
             this.addPanel(new PanelEntityPreview(new GuiTransform(GuiAlign.TOP_LEFT, 0, 16, width, 64, 0), target)
-                    .setRotationDriven(new ValueFuncIO<>(() -> 15F), new ValueFuncIO<>(() ->
-                            (float) (Minecraft.getSystemTime() % 30000L / 30000D * 360D))));
+                    .setRotationDriven(pitch, new ValueFuncIO<>(() -> (float) (Minecraft.getSystemTime() % 30000L / 30000D * 360D))));
+        }
         recalcSizes();
     }
 }
